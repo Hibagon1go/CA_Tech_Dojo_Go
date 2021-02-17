0c8327ad74327a610cd5f25099c2eab008a6a75b
檜原陽一郎 <hibarayoichiro@hibaruyouichirounoMacBook-puro.local>
2021-02-17 20:36:22 +0900

    All required functions cra eated succesfully(before refuctoring)

--- src/api/model/user.go
+++ src/api/model/user.go
@@ -4,8 +4,8 @@ package model
 
 // Userテーブルを定義
 type User struct {
-	Name           string          `gorm:"primary_key" json:"name"`
-	Token          string          `json:"token"`
-	UserID         string          `json:"userID"`
-	UserCharacters []UserCharacter `json:"userCharacters"`
+	Name   string `gorm:"primary_key" json:"name"`
+	Token  string `json:"token"`
+	UserID string `json:"userID"`
+	// UserCharacters []UserCharacter `json:"userCharacters"`
 }
